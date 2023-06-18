a620666f524c51acb1a7fb9669ff5e8fb7cfa45a
aluno23032 <aluno23032@ipt.pt>
2023-06-18 12:56:47 +0100

    MQTT funcional

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import Paho from "paho-mqtt";
+import { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+
+var client = new Paho.Client(
+  "localhost",
+  Number(9001),
+  `mqtt-async-test-${parseInt(Math.random() * 100)}`
+);
 
 export default function App() {
+
+  const [value, setValue] = useState(0);
+
+  function onMessage(message) {
+    if (message.destinationName === "teste")
+        setValue(parseInt(message.payloadString));
+  }
+
+  useEffect(() => {
+    client.connect( {
+      onSuccess: () => { 
+      console.log("Connected!");
+      client.subscribe("teste");
+      client.onMessageArrived = onMessage;
+    },
+    onFailure: () => {
+      console.log("Failed to connect!"); 
+    }
+  });
+  }, [])
+
+  function changeValue(c) {
+    const message = new Paho.Message((value + 1).toString());
+    message.destinationName = "teste";
+    c.send(message);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Value is: {value}</Text>
+      <Button onPress={() => { changeValue(client);} } title="Press Me"/>
       <StatusBar style="auto" />
     </View>
   );
